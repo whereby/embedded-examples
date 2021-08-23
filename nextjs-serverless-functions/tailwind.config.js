3a5c9367e3b831101a594e113b7ed9af42976479
Brian Chen <brianchen8990@gmail.com>
2021-08-23 14:22:55 +0200

    nextjs-vercel: Apply whereby fonts and basic brand style

--- nextjs-serverless-functions/tailwind.config.js
+++ nextjs-serverless-functions/tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      primary: "#2b189b",
+      secondary: "#006654",
+    },
+    fontFamily: {
+      headline: ["Whereby Text", "Georgia", "serif"],
+      "sub-headline": ["Whereby Condensed", "Georgia", "serif"],
+      text: ["Inter", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
