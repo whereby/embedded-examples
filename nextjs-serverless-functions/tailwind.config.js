5512f333096501d5ba0894b8db58817c71fec8bd
Brian Chen <brianchen8990@gmail.com>
2021-08-24 11:52:42 +0200

    nextjs-vercel: Update tailwind config with colors and forms

--- nextjs-serverless-functions/tailwind.config.js
+++ nextjs-serverless-functions/tailwind.config.js
@@ -1,11 +1,16 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     colors: {
+      white: colors.white,
+      gray: colors.coolGray,
       primary: "#2b189b",
       secondary: "#006654",
+      negative: "#c35037",
     },
     fontFamily: {
       headline: ["Whereby Text", "Georgia", "serif"],
@@ -16,5 +21,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
